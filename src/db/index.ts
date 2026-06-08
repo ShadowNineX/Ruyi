f41ec7bcd090b588a3beee7769583eac9c968bee
hedgehogform <alexxycarlsson@gmail.com>
2026-06-08 12:42:14 +0200

    Update index.ts

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,12 +1,52 @@
-import mongoose from "mongoose";
+import mongoose, { type ConnectOptions } from "mongoose";
 
 import { env } from "../env";
 import { dbLogger } from "../logger";
 const MONGO_URI = env.MONGO_URI;
 
+function getMongoConnectOptions(uri: string): ConnectOptions {
+  const parsedUri = new URL(uri);
+  const tlsEnabled =
+    parsedUri.searchParams.get("tls") === "true" ||
+    parsedUri.searchParams.get("ssl") === "true";
+  const tlsCAFile =
+    parsedUri.searchParams.get("tlsCAFile") ??
+    parsedUri.searchParams.get("sslCAFile") ??
+    undefined;
+  const tlsAllowInvalidHostnames =
+    parsedUri.searchParams.get("tlsAllowInvalidHostnames") === "true";
+
+  return {
+    ...(tlsEnabled ? { tls: true } : {}),
+    ...(tlsCAFile ? { tlsCAFile } : {}),
+    ...(tlsAllowInvalidHostnames ? { tlsAllowInvalidHostnames: true } : {}),
+  };
+}
+
+function redactMongoUri(uri: string): string {
+  const parsedUri = new URL(uri);
+  if (parsedUri.password) {
+    parsedUri.password = "****";
+  }
+  return parsedUri.toString();
+}
+
 export async function connectDB(): Promise<typeof mongoose> {
+  const connectOptions = getMongoConnectOptions(MONGO_URI);
+
   try {
-    await mongoose.connect(MONGO_URI);
+    dbLogger.info(
+      {
+        mongoUri: redactMongoUri(MONGO_URI),
+        tls: connectOptions.tls ?? false,
+        tlsCAFile: connectOptions.tlsCAFile,
+        tlsAllowInvalidHostnames:
+          connectOptions.tlsAllowInvalidHostnames ?? false,
+      },
+      "Connecting to MongoDB",
+    );
+
+    await mongoose.connect(MONGO_URI, connectOptions);
     dbLogger.info("Connected to MongoDB");
 
     // Force exit on connection errors after initial connect
